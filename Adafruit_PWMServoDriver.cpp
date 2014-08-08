02ab36e431e4bb612440c8dc2e7f6e8b2133d888
Tony DiCola <tony@tonydicola.com>
2014-08-07 17:13:35 -0700

    Fix #11 by scaling frequency by 0.9.

--- Adafruit_PWMServoDriver.cpp
+++ Adafruit_PWMServoDriver.cpp
@@ -40,7 +40,7 @@ void Adafruit_PWMServoDriver::reset(void) {
 void Adafruit_PWMServoDriver::setPWMFreq(float freq) {
   //Serial.print("Attempting to set freq ");
   //Serial.println(freq);
-  
+  freq *= 0.9;  // Correct for overshoot in the frequency setting (see issue #11).
   float prescaleval = 25000000;
   prescaleval /= 4096;
   prescaleval /= freq;
