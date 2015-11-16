f264d49ac155f1d6c4b100378476b7e9ea9f1139
Wayde Nie <niew@mcmaster.ca>
2015-11-16 11:49:17 -0500

    add: begin(int sda, int scl) that passes alternate sda/scl pins to I2C/Wire library for boards like ESP-01

--- Adafruit_PWMServoDriver.h
+++ Adafruit_PWMServoDriver.h
@@ -47,6 +47,7 @@ class Adafruit_PWMServoDriver {
  public:
   Adafruit_PWMServoDriver(uint8_t addr = 0x40);
   void begin(void);
+  void begin(int sda, int scl);
   void reset(void);
   void setPWMFreq(float freq);
   void setPWM(uint8_t num, uint16_t on, uint16_t off);
