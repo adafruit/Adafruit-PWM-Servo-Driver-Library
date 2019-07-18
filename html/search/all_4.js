72fa15e6bdbcbac3a64dcfd426018ffc56f707b3
Travis CI <travis@travis-ci.org>
2019-07-18 16:20:45 +0000

    Deploy code docs to GitHub Pages Travis build: 35
    Commit: ef1983b952dde981ad6a66c9e176ce686d26056f

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['pca9685_5fmode1',['PCA9685_MODE1',['../_adafruit___p_w_m_servo_driver_8h.html#aec642e3f25e7f83072d68acb14ae4e74',1,'Adafruit_PWMServoDriver.h']]],
+  ['pca9685_5fmode2',['PCA9685_MODE2',['../_adafruit___p_w_m_servo_driver_8h.html#adcea88f055037b45998a614ae3cf492d',1,'Adafruit_PWMServoDriver.h']]],
   ['pca9685_5fprescale',['PCA9685_PRESCALE',['../_adafruit___p_w_m_servo_driver_8h.html#a7175106bbec978d9acc85dc7485235a3',1,'Adafruit_PWMServoDriver.h']]],
   ['pca9685_5fsubadr1',['PCA9685_SUBADR1',['../_adafruit___p_w_m_servo_driver_8h.html#ad58a880e68f5aa69092c658a86641a8e',1,'Adafruit_PWMServoDriver.h']]],
   ['pca9685_5fsubadr2',['PCA9685_SUBADR2',['../_adafruit___p_w_m_servo_driver_8h.html#a845ec28ef89ed81642ea5f41691d4b51',1,'Adafruit_PWMServoDriver.h']]],
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['setextclk',['setExtClk',['../class_adafruit___p_w_m_servo_driver.html#a7967e51188cbacf2c3f269c8aec9d292',1,'Adafruit_PWMServoDriver']]],
+  ['setoutputmode',['setOutputMode',['../class_adafruit___p_w_m_servo_driver.html#a8d18d478574c8c686e94faefe11094b9',1,'Adafruit_PWMServoDriver']]],
   ['setpin',['setPin',['../class_adafruit___p_w_m_servo_driver.html#a1246cd50849fe0f068cc5d474e06ae96',1,'Adafruit_PWMServoDriver']]],
   ['setpwm',['setPWM',['../class_adafruit___p_w_m_servo_driver.html#a724a7fc39c6fba34478ecc0eea038bd3',1,'Adafruit_PWMServoDriver']]],
   ['setpwmfreq',['setPWMFreq',['../class_adafruit___p_w_m_servo_driver.html#a0ef6f1e3c81aebbd1d1da1bb12f3ed5c',1,'Adafruit_PWMServoDriver']]],
