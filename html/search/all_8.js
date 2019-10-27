3b6fdc1649e849b7e34383f2674a9b597c9e2e45
Travis CI <travis@travis-ci.org>
2019-10-27 23:00:04 +0000

    Deploy code docs to GitHub Pages Travis build: 61
    Commit: c9b843a6aff7dc95b7e55c8274f28c782e061483

--- html/search/all_8.js
+++ html/search/all_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['wakeup',['wakeup',['../class_adafruit___p_w_m_servo_driver.html#a34b9ac672853a4a1b7fc202b4c89d8c4',1,'Adafruit_PWMServoDriver']]]
+  ['wakeup',['wakeup',['../class_adafruit___p_w_m_servo_driver.html#a34b9ac672853a4a1b7fc202b4c89d8c4',1,'Adafruit_PWMServoDriver']]],
+  ['writemicroseconds',['writeMicroseconds',['../class_adafruit___p_w_m_servo_driver.html#aa91cf057ec01505292401e4fdceb57ac',1,'Adafruit_PWMServoDriver']]]
 ];
