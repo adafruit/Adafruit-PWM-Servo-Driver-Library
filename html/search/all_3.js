bff9388bab548e5e072da9b8002b6422e54ca160
Travis CI <travis@travis-ci.org>
2019-11-03 19:06:37 +0000

    Deploy code docs to GitHub Pages Travis build: 76
    Commit: 16705a539bfe829ad518f6f69ffcbb3a1c087181

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['getoscillatorfrequency',['getOscillatorFrequency',['../class_adafruit___p_w_m_servo_driver.html#ae4017d3cbbda98a0bb41efcf2c1fcf30',1,'Adafruit_PWMServoDriver']]],
   ['getpwm',['getPWM',['../class_adafruit___p_w_m_servo_driver.html#a387493a29e513ef700cfb4de30c0acc7',1,'Adafruit_PWMServoDriver']]]
 ];
--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['setextclk',['setExtClk',['../class_adafruit___p_w_m_servo_driver.html#a7967e51188cbacf2c3f269c8aec9d292',1,'Adafruit_PWMServoDriver']]],
+  ['setoscillatorfrequency',['setOscillatorFrequency',['../class_adafruit___p_w_m_servo_driver.html#ac725dd6b0d24d087586a854e81bcf6b3',1,'Adafruit_PWMServoDriver']]],
   ['setoutputmode',['setOutputMode',['../class_adafruit___p_w_m_servo_driver.html#a8d18d478574c8c686e94faefe11094b9',1,'Adafruit_PWMServoDriver']]],
   ['setpin',['setPin',['../class_adafruit___p_w_m_servo_driver.html#a1246cd50849fe0f068cc5d474e06ae96',1,'Adafruit_PWMServoDriver']]],
   ['setpwm',['setPWM',['../class_adafruit___p_w_m_servo_driver.html#a724a7fc39c6fba34478ecc0eea038bd3',1,'Adafruit_PWMServoDriver']]],
