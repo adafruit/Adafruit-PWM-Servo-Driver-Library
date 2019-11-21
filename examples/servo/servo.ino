155284da2a47424b047d7c1fef23a6415953e0dd
Walt Sorensen <photodude@users.noreply.github.com>
2019-11-20 17:54:19 -0700

    Analog servos run at ~50 Hz updates

--- examples/servo/servo.ino
+++ examples/servo/servo.ino
@@ -49,7 +49,7 @@ void setup() {
   // that precise. You can 'calibrate' by tweaking this number till
   // you get the frequency you're expecting!
   pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz  
-  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~60 Hz updates
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
   delay(10);
 }
