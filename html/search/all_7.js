173a53b82f8ed29e7001bbf135e3e23b00e3abb2
Doxygen CI <ci-arduino@invalid>
2022-06-28 21:34:39 +0000

    Deploy docs to GitHub Pages from commit 18f9b30556
    Commit: 18f9b3055679c0cb7ea72ed1b6e8e249ffb6808e
    GitHub Actions run: 2579234731

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -4,7 +4,7 @@ var searchData=
   ['setoscillatorfrequency',['setOscillatorFrequency',['../class_adafruit___p_w_m_servo_driver.html#ac725dd6b0d24d087586a854e81bcf6b3',1,'Adafruit_PWMServoDriver']]],
   ['setoutputmode',['setOutputMode',['../class_adafruit___p_w_m_servo_driver.html#a8d18d478574c8c686e94faefe11094b9',1,'Adafruit_PWMServoDriver']]],
   ['setpin',['setPin',['../class_adafruit___p_w_m_servo_driver.html#a1246cd50849fe0f068cc5d474e06ae96',1,'Adafruit_PWMServoDriver']]],
-  ['setpwm',['setPWM',['../class_adafruit___p_w_m_servo_driver.html#a724a7fc39c6fba34478ecc0eea038bd3',1,'Adafruit_PWMServoDriver']]],
+  ['setpwm',['setPWM',['../class_adafruit___p_w_m_servo_driver.html#a3f400e2cc3df479ed98d0ce7e7a0860d',1,'Adafruit_PWMServoDriver']]],
   ['setpwmfreq',['setPWMFreq',['../class_adafruit___p_w_m_servo_driver.html#a0ef6f1e3c81aebbd1d1da1bb12f3ed5c',1,'Adafruit_PWMServoDriver']]],
   ['sleep',['sleep',['../class_adafruit___p_w_m_servo_driver.html#aa892432ed08e4b3892c8eb0478168dd8',1,'Adafruit_PWMServoDriver']]]
 ];
